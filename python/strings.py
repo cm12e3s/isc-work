f257704a51f1607ec280fe3e6caf839ca685633a
user01 <cm12e3s@leeds.ac.uk>
2017-03-01 14:29:40 +0000

    Completed strings exercise, with question 3.

--- python/strings.py
+++ python/strings.py
@@ -3,17 +3,34 @@ for i in s:
     print i
 
 print s[4]
-
 print s[-1]
-
 print len(s)
-
 print s[0] + "\n", s[0][0][0]
 
 split_s = s.split()
 print split_s
 for word in split_s:
-    if word.find ("i") > -1 or word.find("I") > -1:
+    if word.find ("i") > -1:
         print "I found 'i' in: '{}'".format(word)
+    elif word.find ("I") > -1:
+        print "I found 'I' in: '{}'".format(word)
+    else:
+        print "Letters 'i' and 'I' not found in '{}'".format(word)
+
+something = "Completely different"
+print dir(something)
+print something.count("t")
+print something.find ("plete")
+print something.split("e")
+thing2 = something.replace("Different", "Silly") # this will not work, strings are immutable so can't be changed.
+print thing2
+something[0] = "B"
+print something
+
+
+
+
+
+
 
 
