bbb6c5f15d5baa7510d9b1d2442fd367b47792d7
user01 <cm12e3s@leeds.ac.uk>
2017-03-03 14:17:57 +0000

    My plans for japan.

---
+++ python/japan.py
@@ -0,0 +1,8 @@
+Meji-jingu - shrine
+Takeshita-dori - pop culture bazaar
+Omote-sando - archtecture
+Shinjuki - noodle and drink
+
+Tsukiji Market - fish market
+Hama-rikyu Onshiteien - garden
+Ginza - stores and art galleries.
