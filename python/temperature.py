8fcfde6e4998845dd73e8645aa6c460370e094cd
user01 <cm12e3s@leeds.ac.uk>
2017-03-03 11:19:17 +0000

    Completed task for data acquistion with instrument.

--- python/temperature.py
+++ python/temperature.py
@@ -1,13 +1,28 @@
-import serial 
+from datetime import datetime
+import serial, io
+
+outfile = 'serial-temperature.tsv'
 
 ser = serial.Serial(
     port = '/dev/ttyUSB0',
     baudrate = 9600,
     bytesize = serial.EIGHTBITS,
     parity = serial.PARITY_NONE,
-    stopbits = serial.STOPBITS_one
+    stopbits = serial.STOPBITS_ONE
 )
 
-print ser.read(size=8)
-ser.close
+sio = io.TextIOWrapper(
+    io.BufferedRWPair (ser, ser, 1),
+    encoding = 'ascii', newline='\r'
+)
 
+with open (outfile, 'a') as f: #appends to exsisting file
+    while ser.isOpen():
+        datastring = sio.readline()
+        f.write (datetime.utcnow().isoformat() + '\t' + datastring + '\n')
+        f.flush() # included to force the system to write to disk.
+
+ser.close()
+
+
+    
